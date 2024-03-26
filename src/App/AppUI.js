791307d4e98ccbfd92fec6beec97ca53e7514f8e
Divar Parra <arc.eseisnos@gmail.com>
2024-03-26 15:02:43 -0500

    Initial commit

---
+++ src/App/AppUI.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoContext } from '../TodoContext';
+import { Modal } from '../Modal';
+import { TodoForm } from '../TodoForm';
+
+function AppUI() {
+
+  const {
+    loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext)
+
+  return (
+    <div className="app-container">
+      <h1 className='app-title'>¡Machine TODO en REACT!</h1>
+      <TodoCounter />
+      <TodoSearch />
+
+          <TodoList>
+            {loading && (
+              <>
+                <TodosLoading />
+                <TodosLoading />
+                <TodosLoading />
+              </>
+            )}
+            {error && <TodosError/>}
+            {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
+
+            {searchedTodos.map(todo => (
+              <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </TodoList>
+      
+      <CreateTodoButton 
+        setOpenModal={setOpenModal} 
+      />
+
+      {openModal && (
+        <Modal>
+            <TodoForm />
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export { AppUI };
